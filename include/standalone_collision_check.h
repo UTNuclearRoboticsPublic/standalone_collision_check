ced13e9e9840323b49d953906250215e176a0bae
AndyZe <andyz@utexas.edu>
2017-08-15 15:44:23 -0500

    Trying several methods to get the minimum collision distance.

--- include/standalone_collision_check.h
+++ include/standalone_collision_check.h
@@ -3,6 +3,10 @@
 #define STANDALONE_COLLISION_CHECK_H
 
 #include <moveit/planning_scene/planning_scene.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
 #include <moveit/robot_model_loader/robot_model_loader.h>
+#include <ros/ros.h>
+
+void spawn_collision_cube(ros::NodeHandle& nh);
 
 #endif
--- src/standalone_collision_check.cpp
+++ src/standalone_collision_check.cpp
@@ -3,11 +3,12 @@
 
 #include <standalone_collision_check.h>
 
-
 int main(int argc, char** argv) {
 
   ros::init (argc, argv, "collision_check");
   ros::NodeHandle nh;
+
+  ros::Duration(1.0).sleep();
 
   // Set up the MoveIt scene
   robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
@@ -18,10 +19,30 @@ int main(int argc, char** argv) {
   robot_state::RobotState robot_state = planning_scene.getCurrentState();
   collision_detection::AllowedCollisionMatrix acm = planning_scene.getAllowedCollisionMatrix();
 
-
+  ros::Duration(10).sleep();
   // Spawn a virtual collision object (for testing)
-  ros::Publisher planning_scene_diff_publisher = nh.advertise<moveit_msgs::PlanningScene>("planning_scene", 1);
-  while(planning_scene_diff_publisher.getNumSubscribers() < 1)
+  spawn_collision_cube(nh);
+
+  // Spin while checking minimum collision distance
+  while ( ros::ok() )
+  {
+    collision_result.clear();
+    robot_state = planning_scene.getCurrentState();
+    //planning_scene.checkCollision(collision_request, collision_result, robot_state, acm);
+    //ROS_INFO_STREAM("Current state is " << (collision_result.collision ? "in" : "not in") << " collision");
+    ROS_INFO_STREAM("Minimum distance is " << planning_scene.distanceToCollision( robot_state ) << ".");
+
+    ros::Duration(1.).sleep();
+  }
+
+  return 0;
+}
+
+
+void spawn_collision_cube(ros::NodeHandle& nh)
+{
+  ros::Publisher collision_object_publisher = nh.advertise<moveit_msgs::CollisionObject>("collision_object", 1);
+  while(collision_object_publisher.getNumSubscribers() < 1)
   {
     ros::Duration(0.1).sleep();
   }
@@ -29,33 +50,24 @@ int main(int argc, char** argv) {
   moveit_msgs::CollisionObject collision_object;
   collision_object.header.frame_id = "base_link";
   collision_object.id = "box";
-  geometry_msgs::Pose pose;
-  pose.orientation.w = 1.0;
+
   shape_msgs::SolidPrimitive primitive;
   primitive.type = primitive.BOX;
   primitive.dimensions.resize(3);
-  primitive.dimensions[0] = 0.5;
-  primitive.dimensions[1] = 0.5;
-  primitive.dimensions[2] = 0.5;
-  collision_object.primitives.push_back(primitive);
-  collision_object.primitive_poses.push_back(pose);
+  primitive.dimensions[0] = 0.05;
+  primitive.dimensions[1] = 0.05;
+  primitive.dimensions[2] = 0.05;
+  collision_object.primitives.resize(1);
+  collision_object.primitives[0] = primitive;
 
-  planning_scene.world.collision_objects.push_back(collision_object.object);
-  planning_scene.is_diff = true;
-  planning_scene_diff_publisher.publish(planning_scene);
-  ros::Duration(0.1).sleep();
+  geometry_msgs::Pose pose;
+  pose.position.x = 0.8;
+  pose.orientation.w = 1.0;
+  collision_object.primitive_poses.resize(1);
+  collision_object.primitive_poses[0] = pose;
 
+  collision_object.operation = collision_object.ADD;
+  collision_object_publisher.publish( collision_object );
 
-  // Spin while checking minimum collision distance
-  while ( ros::ok() )
-  {
-    collision_result.clear();
-    robot_state = planning_scene.getCurrentState();
-    planning_scene.checkCollision(collision_request, collision_result, robot_state, acm);
-    ROS_INFO_STREAM("Current state is " << (collision_result.collision ? "in" : "not in") << " self collision");
-
-    ros::Duration(0.05).sleep();
-  }
-
-  return 0;
+  return;
 }
