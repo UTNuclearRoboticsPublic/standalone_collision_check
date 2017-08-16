fa3f67a0acf0f77034f41676e66ec3ac33b896d9
AndyZe <andyz@utexas.edu>
2017-08-16 16:07:03 -0500

    A configurable launch file.

--- include/standalone_collision_check.h
+++ include/standalone_collision_check.h
@@ -2,11 +2,33 @@
 #ifndef STANDALONE_COLLISION_CHECK_H
 #define STANDALONE_COLLISION_CHECK_H
 
-#include <moveit/planning_scene/planning_scene.h>
-#include <moveit/planning_scene_interface/planning_scene_interface.h>
-#include <moveit/robot_model_loader/robot_model_loader.h>
 #include <ros/ros.h>
+#include "std_msgs/Bool.h"
+#include <string> 
 
+// MoveIt!
+#include <moveit/planning_scene/planning_scene.h>
+#include <moveit/kinematic_constraints/utils.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <eigen_conversions/eigen_msg.h>
+
+// Update robot joints
+void jointCallback(const sensor_msgs::JointState &msg);
+
+// Spawn a cube for testing
 void spawn_collision_cube(ros::NodeHandle& nh);
+
+// Read launch file arguments
+void read_launch_args(ros::NodeHandle& nh);
+
+namespace standalone_collision_check
+{
+  int g_num_joints = 6;  // Read from launch file
+  std::vector<double> g_current_joints;
+
+  bool g_test_with_cube = false;  // Read from launch file
+  bool g_test_with_random_joints = false;  // Read from launch file
+}
+using namespace standalone_collision_check;
 
 #endif
--- src/standalone_collision_check.cpp
+++ src/standalone_collision_check.cpp
@@ -1,5 +1,5 @@
 
-// Refer to https://github.com/ros-planning/moveit_tutorials/blob/kinetic-devel/doc/pr2_tutorials/interactivity/src/collision_contact_tutorial.cpp
+// Refer to http://docs.ros.org/indigo/api/moveit_tutorials/html/doc/pr2_tutorials/planning/src/doc/planning_scene_tutorial.html
 
 #include <standalone_collision_check.h>
 
@@ -7,41 +7,66 @@ int main(int argc, char** argv) {
 
   ros::init (argc, argv, "collision_check");
   ros::NodeHandle nh;
+  ros::Subscriber sub = nh.subscribe("joint_states", 5, jointCallback);
 
-  // Set up the MoveIt scene
   robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
   robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
   planning_scene::PlanningScene planning_scene(kinematic_model);
-  robot_state::RobotState robot_state = planning_scene.getCurrentState();
-  collision_detection::AllowedCollisionMatrix acm = planning_scene.getAllowedCollisionMatrix();
+
+  collision_detection::CollisionRequest collision_request;
+  collision_detection::CollisionResult collision_result;
+
+  robot_state::RobotState& current_state = planning_scene.getCurrentStateNonConst();
+
+  // Read string arguments from launch file, then convert to proper data types
+  ros::NodeHandle pn("~");
+  read_launch_args(pn);
 
   // Spawn a virtual collision object (for testing)
-  //spawn_collision_cube(nh);
+  if (g_test_with_cube)
+    spawn_collision_cube(nh);
 
   // Spin while checking minimum collision distance
   while ( ros::ok() )
   {
-    //process collision objects in scene
-    moveit::planning_interface::PlanningSceneInterface planning_scene_interface_;
-    std::map<std::string, moveit_msgs::CollisionObject> c_objects_map = planning_scene_interface_.getObjects();
-    for(auto& kv : c_objects_map){
-      planning_scene.processCollisionObjectMsg(kv.second);
-    }
+    //ROS_INFO_STREAM( g_current_joints.at(0) <<"  " << g_current_joints.at(1) );
+    current_state = planning_scene.getCurrentStateNonConst();
 
-  ROS_INFO("");
-  ROS_INFO_STREAM("Distance to Collision: " << planning_scene.distanceToCollision(robot_state));
-    ROS_INFO_STREAM("Is state colliding: " << planning_scene.isStateColliding());
-  ROS_INFO("");
+    // For testing: overwrite actual joint values with randoms
+    if (g_test_with_random_joints)
+      current_state.setToRandomPositions();
+    else
+      current_state.setVariablePositions(g_current_joints);
 
-    ros::Duration(1.).sleep();
+    collision_result.clear();
+
+    planning_scene.checkCollision(collision_request, collision_result);
+    ROS_INFO_STREAM("Current state is "
+                  << (collision_result.collision ? "in" : "not in")
+                  << " collision");
+
+    ros::spinOnce();
+    ros::Duration(.02).sleep();  // Spin at 50 Hz
   }
 
   return 0;
 }
 
 
+void jointCallback(const sensor_msgs::JointState &msg)
+{
+  for (int i=0; i<g_num_joints; i++)
+    g_current_joints.at(i) = msg.position[i];
+  
+  return;
+}
+
+
+// Spawn a collision cube for test purposes
 void spawn_collision_cube(ros::NodeHandle& nh)
 {
+  ROS_INFO_STREAM("Spawning a collision cube...");
+
   ros::Publisher collision_object_publisher = nh.advertise<moveit_msgs::CollisionObject>("collision_object", 1);
   while(collision_object_publisher.getNumSubscribers() < 1)
   {
@@ -69,6 +94,39 @@ void spawn_collision_cube(ros::NodeHandle& nh)
 
   collision_object.operation = collision_object.ADD;
   collision_object_publisher.publish( collision_object );
+
+  return;
+}
+
+
+// Read string arguments from launch file, then convert to proper data types
+void read_launch_args(ros::NodeHandle& nh)
+{
+  while (!nh.hasParam("test_with_cube"))
+  {
+    ros::Duration(0.1).sleep();
+  }
+  nh.param<bool>("test_with_cube", g_test_with_cube, false);
+  while (!nh.hasParam("test_with_random_joints"))
+  {
+    ros::Duration(0.1).sleep();
+  }
+  nh.param<bool>("test_with_random_joints", g_test_with_random_joints, false);
+  while (!nh.hasParam("num_joints"))
+  {
+    ros::Duration(0.1).sleep();
+  }
+  nh.param<int>("num_joints", g_num_joints, 6);
+
+
+  ROS_INFO_STREAM("Configuration from launch file: ");
+  ROS_INFO_STREAM("Number of joints: " << g_num_joints);
+  //ROS_INFO( "%d", g_test_with_cube );
+  //ROS_INFO( "%d", g_test_with_random_joints );
+  ROS_INFO_STREAM( (g_test_with_cube ? "test with virtual cube" : "no virtual collision cube will be added") );
+  ROS_INFO_STREAM( (g_test_with_random_joints ? "test with random joints" : "use actual robot joints") );
+
+  g_current_joints.resize(g_num_joints);
 
   return;
 }
